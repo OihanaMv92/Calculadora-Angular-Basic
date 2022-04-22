4af888aefb79d70f47409c043fead8325dc36b51
Oihana Mendez <oihana@gainkode.com>
2022-04-22 19:42:38 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import { setupMaster } from 'cluster';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculadora';
+  title = 'Mini Calculadora';
+  // numero1:string='0';
+  numero1:number=0;
+  numero2:number=0;
+  resultado:number=0;
+
+  // METODO
+  sumar():void{
+  //PARSEAR PARA QUE PASE A ENTERO.
+    // this.resultado= parseInt(this.numero1)+parseInt(this.numero2);
+this.resultado= this.numero1+this.numero2;
+  }
+
+  restar():void{
+    this.resultado= this.numero1-this.numero2;
+      }
+    
+
+    multiplicar():void{
+      this.resultado= this.numero1*this.numero2;
+        }
+
+
+        dividir():void{
+          this.resultado= this.numero1/this.numero2;
+            }
+      
+
 }
+
+
+
